e3d9787f647bdeb449ebce66a474f8995a812a24
Asim <salimli.asim@gmail.com>
2019-03-01 08:40:31 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,48 +3,55 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import keys from "./config/keys";
  
-class App extends React.Component {
+ class App extends React.Component {
     constructor(props) {
         super(props);
         // Defining initial State
-        this.state={
+         this.state={
             lat:null,
             lng:null,
             cel:null,
             far:null,
-            city:null
+            city:null,
+            sky:null,
+            humid: null,
+            desc:null,
+            country:null
         }
-        // Getting User's current location information from window object.
+        // Getting User's current location information from window object and passing it as a  'position' object to callback function.
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                
-                $.ajax({
+            async (position) => {
+        // Making an API call to get weather data.
+              let data_weather = await $.ajax({
               url: 'https://weather.cit.api.here.com/weather/1.0/report.json',
               type: 'GET',
               dataType: 'jsonp',
               jsonp: 'jsonpcallback',
               data: {
                     product: 'observation',
-                    latitude: 40.4093,
-                    longitude: 49.8671,
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
                     oneobservation: 'true',
                     app_id: keys.app_id,
                     app_code: keys.app_code
                 },
                 success: function (data) {
+                    return data;
                     console.log(JSON.stringify(data.observations.location[0].observation[0]));
-                    console.log(JSON.stringify(data.observations.location[0].observation[0].skyDescription));
-                    console.log(JSON.stringify(data.observations.location[0].observation[0].humidity));
-                    console.log(JSON.stringify(data.observations.location[0].observation[0].temperatureDesc));
-                    console.log(JSON.stringify(data.observations.location[0].observation[0].iconLink));
-                    console.log(data.observations.location[0].observation[0].temperature);
-                    console.log( (data.observations.location[0].observation[0].temperature*9/5) + 32 );
-                }
+                     // console.log(JSON.stringify(data.observations.location[0].observation[0].humidity));
+                     // console.log(JSON.stringify(data.observations.location[0].observation[0].iconLink));
+                 }
             });
-            this.setState({lat: position.coords.latitude})
-
-                console.log(position.coords.latitude, position.coords.longitude )
-            },
+            console.log(data_weather.observations.location[0].observation[0])
+            this.setState({lat: position.coords.latitude});
+            this.setState({lng: position.coords.longitude});
+            this.setState({cel: data_weather.observations.location[0].observation[0].temperature });
+            this.setState({far: (data_weather.observations.location[0].observation[0].temperature*9/5) + 32 });
+            this.setState({city: position.coords.latitude});
+            this.setState( {sky: JSON.stringify(data_weather.observations.location[0].observation[0].skyDescription) });
+            this.setState({desc: JSON.stringify(data_weather.observations.location[0].observation[0].temperatureDesc) });
+            this.setState({country: position.coords.longitude});
+             },
             (err) => { console.log(err); alert('Please select city manualy or change location permissions') }
         )
     }
@@ -52,37 +59,16 @@ class App extends React.Component {
         return (
             <div>
                 <h2> Latitude: {this.state.lat} </h2>
-                <h2> Temp C: </h2>
-                <h2> Temp F: </h2>
-                <h2> Sky: </h2>
-                <h2> Weather status: </h2>
+                 <h2> Latitude: {this.state.lng} </h2>
+                <h2> Temp C: {this.state.cel} </h2>
+                <h2> Temp F: {this.state.far} </h2>
+                <h2> Sky: {this.state.sky} </h2>
+                <h2> Weather status: {this.state.desc} </h2>
             </div>
             )
     } 
 }
-    $.ajax({
-              url: 'https://weather.cit.api.here.com/weather/1.0/report.json',
-              type: 'GET',
-              dataType: 'jsonp',
-              jsonp: 'jsonpcallback',
-              data: {
-                    product: 'observation',
-                    latitude: 40.4093,
-                    longitude: 49.8671,
-                    oneobservation: 'true',
-                    app_id: 'lLg8lP3cuRb0KFN4aUVR',
-                    app_code: 'MgkUAlU8bwpPAAKhm3ZXyw'
-                },
-                success: function (data) {
-                    console.log(JSON.stringify(data.observations.location[0].observation[0]));
-                    console.log(JSON.stringify(data.observations.location[0].observation[0].skyDescription));
-                    console.log(JSON.stringify(data.observations.location[0].observation[0].humidity));
-                    console.log(JSON.stringify(data.observations.location[0].observation[0].temperatureDesc));
-                    console.log(JSON.stringify(data.observations.location[0].observation[0].iconLink));
-                    console.log(data.observations.location[0].observation[0].temperature);
-                    console.log( (data.observations.location[0].observation[0].temperature*9/5) + 32 );
-                }
-            });
+     
     
  
 ReactDOM.render(<App />, document.getElementById('root'));
